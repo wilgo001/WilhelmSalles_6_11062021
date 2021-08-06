c5c10e7d57ec5460a63c070ddc95023aac631f09
wilgo001 <wilhelm.salles@outlook.fr>
2021-08-06 17:17:17 +0200

    feature : fonctionnal buttons on photo full screen

--- components/PhotoFullScreen/PhotoFullScreen.js
+++ components/PhotoFullScreen/PhotoFullScreen.js
@@ -5,6 +5,7 @@ class PhotoFullScreen extends Component{
     arrowLeft;
     arrowRight;
     img;
+    photo;
 
     constructor(parent) {
         super(parent);
@@ -12,11 +13,11 @@ class PhotoFullScreen extends Component{
 
     buildUI() {
         this.containerFull = uDom.CE('div', {className: 'full-screen container-full'});
-        this.closeBtn = uDom.CE('i', {className: 'full-sreen close-btn'});
+        this.closeBtn = uDom.CE('i', {className: 'full-screen close-btn'});
         this.arrowLeft = uDom.CE('i', {className: 'full-screen left-btn'});
         this.arrowRight = uDom.CE('i', {className: 'full-screen right-btn'});
-        this.containerImg = uDom.CE('div', {className: 'full-screen container-img'});
-        this.containerSec = uDom.AC(uDom.CE('div', {className: 'full-screen container-sec'}), this.closeBtn, this.arrowLeft, this.containerImg, this.arrowRight);
+        this.containerImg = uDom.CE('span', {className: 'full-screen container-img'});
+        this.containerSec = uDom.AC(uDom.CE('span', {className: 'full-screen container-sec'}), this.closeBtn, this.arrowLeft, this.containerImg, this.arrowRight);
 
         this.closeBtn.addEventListener('click', (e)=>{this.closeFullScreen()});
         this.arrowLeft.addEventListener('click', (e)=>{this.prevImg()});
@@ -26,8 +27,9 @@ class PhotoFullScreen extends Component{
     }
 
     showImg(photo) {
+        this.photo = photo;
         if(photo.isImg)
-            this.img = uDom.CE('img', {src: '../..'+ photo.src, className: 'full-sreen img', alt: this.title});
+            this.img = uDom.CE('img', {src: '../..'+ photo.src, className: 'full-screen img', alt: this.title});
         else
             this.img = uDom.CE('video', {src: '../..'+ photo.src, className: 'full-screen img', type: 'video/mp4'});
         uDom.AC(this.containerImg, this.img);
@@ -35,14 +37,27 @@ class PhotoFullScreen extends Component{
     }
 
     closeFullScreen() {
-
+        this.containerFull.classList.remove('open');
+        this.containerImg.innerHTML = '';
     }
 
     prevImg() {
-
+        this.closeFullScreen();
+        let index = dataManager.photoList.indexOf(this.photo);
+        index--;
+        if(index < 0)
+            this.showImg(dataManager.photoList[dataManager.photoList.length-1]);
+        else
+            this.showImg(dataManager.photoList[index]);
     }
 
     nextImg() {
-
+        this.closeFullScreen();
+        let index = dataManager.photoList.indexOf(this.photo);
+        index++;
+        if(index >= dataManager.photoList.length)
+            this.showImg(dataManager.photoList[0]);
+        else
+            this.showImg(dataManager.photoList[index]);
     }
 }
