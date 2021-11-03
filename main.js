481f4610e6a0c2a318ee26b8608131ab11d4481a
wilgo001 <wilhelm.salles@outlook.fr>
2021-11-03 11:54:21 +0100

    show up btn

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ const start = () => {
     dataManager.tagParentElement = document.querySelector('nav');
     dataManager.photographerParentElement = document.querySelector('main');
     dataManager.startHome();
+    document.addEventListener('scroll', (e) => {
+        if(document.scrollingElement.scrollTop >= 1) {
+            document.getElementById('scroll-button').classList.remove('display-none');
+        } else {
+            document.getElementById('scroll-button').classList.add('display-none');
+        }
+    })
+}
+
+const scrollEvent = () => {
+    document.scrollingElement.scrollTop = 0;
+    document.getElementById('scroll-button').classList.add('dipslay-none');
 }
